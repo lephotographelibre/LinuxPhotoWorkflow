d433c9cd09cdbe11f2c1f89bf5a3bddc39c1bb1c
lephotographelibre <lephotgraphelibre@yahoo.com>
2019-01-08 11:10:39 +0100

    V1.0 Initial file from Linux Photography 19.01.01 - Dmitri Popov

---
+++ exiftool-helper.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# From: Linux Photography 19.01.01 - Dmitri Popov
+#
+while getopts "h?sdyar" opt; do
+case $opt in
+    s) exiftool -directory=%e .
+    ;;
+    d) exiftool -d %Y%m%d-%H%M%S.%%e '-FileName<DateTimeOriginal' -r .
+    ;;
+    y) exiftool '-Directory<CreateDate' -d %Y-%m-%d -r .
+    ;;
+    a) exiftool -T -r -filename -make -model -fnumber -exposuretime -iso -r . > results.txt
+    ;;
+    r) exiftool -overwrite_original -all= -r .
+    ;;
+    h|\?)
+    cat <<EOF
+  USAGE:
+    $0 [OPTIONS]
+    $0 -s sort photos into folders by file extension
+    $0 -d rename photos by date
+    $0 -y sort photos into folders by year, month, and date
+    $0 -a save key EXIF data in the results.txt file
+    $0 -r remove all EXIF metadata from photos
+  EOF
+    exit 2
+  ;;
+  esac
+  done
