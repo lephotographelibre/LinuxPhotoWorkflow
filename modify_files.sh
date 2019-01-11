93455056c1137329a35aac1bfd1b64dc94112e02
lephotographelibre <lephotgraphelibre@yahoo.com>
2019-01-11 15:19:27 +0100

    V0.1 JMD -c Convert file from png to jpg

---
+++ modify_files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# From: Linux Photography 19.01.01 - Dmitri Popov
+#       V0.1 JMD -c Convert file from png to jpg
+#
+while getopts "h?c" opt; do
+  case $opt in
+    c) echo "*** Conversion de "${2%.*}.png "--->" ${2%.*}.jpg
+    convert ${2%.*}.png  -quality 90 ${2%.*}.jpg
+    ls -al ${2%.*}.*
+      ;;
+    h|\?)
+      echo "USAGE: -c "
+      exit 2
+      ;;
+  esac
+done
