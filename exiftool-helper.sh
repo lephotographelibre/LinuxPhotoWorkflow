bf23b50393d9d353015606ab87eda5d61218c91e
lephotographelibre <lephotgraphelibre@yahoo.com>
2019-01-11 14:50:22 +0100

    V1.1 Beautified

--- exiftool-helper.sh
+++ exiftool-helper.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 #
 # From: Linux Photography 19.01.01 - Dmitri Popov
+#       V1.1 JLM sorts photos into directories named by the original
 #
 while getopts "h?sdyare" opt; do
-case $opt in
+  case $opt in
     s) exiftool -directory=%e .
-    ;;
+      ;;
     d) exiftool -d %Y%m%d-%H%M%S.%%e '-FileName<DateTimeOriginal' -r .
-    ;;
+      ;;
     y) exiftool '-Directory<CreateDate' -d %Y-%m-%d -r .
-    ;;
+      ;;
     a) exiftool -T -r -filename -make -model -fnumber -exposuretime -iso -r . > results.txt
-    ;;
+      ;;
     r) exiftool -overwrite_original -all= -r .
-    ;;
+      ;;
     e) exiftool -directory=%e DIR
-    ;;
+      ;;
     h|\?)
-    cat <<EOF
+      cat <<EOF
   USAGE:
     $0 [OPTIONS]
     $0 -s sort photos into folders by file extension
@@ -27,7 +28,7 @@ case $opt in
     $0 -r remove all EXIF metadata from photos
     $0 -e sorts photos into directories named by the original extension
   EOF
-    exit 2
-  ;;
+      exit 2
+      ;;
   esac
-  done
+done
