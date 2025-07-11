68012f1d1a215bd0e5866d01c36bbe577d8b8b25
lephotographelibre <lephotographelibre@users.noreply.github.com>
2025-07-11 16:00:16 +0200

    Create convertToJPEG.sh

---
+++ convertToJPEG.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# from: aurelien pierre
+# Modifie pour le web toutes les images d'une extension donnée
+# Parcourt les dossiers et sous-dossiers récursivement
+# Compresse en JPEG optimisé et accentue la netteté
+# Appel : sh crop.sh EXTENSION
+
+FILES=$(ls -R *.$1)
+
+for FILE in $FILES
+	do
+	convert $FILE -resize 2048x1600 -unsharp 2x0.5+0.5+0 "${FILE%%.*}"-web.jpg
+	jpegoptim --all-progressive "${FILE%%.*}"-web.jpg
+done
